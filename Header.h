5fa6a7ab8385ce6564f3b3bcda78ab72c7116cf2
EvanKaraf <vaggeliskaraf@gmail.com>
2016-07-04 04:36:32 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,37 @@
+#ifndef BINARY_TREE_HEADER
+#define BINARY_TREE_HEADER
+#include "myforum.h"
+#include <list>
+
+typedef post ItemType;
+
+class ListNode{
+public:
+	ItemType currPost;
+	ListNode* next;
+};
+
+class TreeNode {
+public:
+	std::string creator;
+	std::list<post> creatorsPost;
+	TreeNode* right;
+	TreeNode* left;
+};
+
+class btree {
+public:
+	void insert(ItemType);
+	void destroy();
+	TreeNode* search(ItemType);
+	void printInOrder();
+	btree();
+
+private:
+	TreeNode* insert(TreeNode*, ItemType);
+	void destroy(TreeNode*);
+	TreeNode* search(TreeNode*, ItemType);
+	void printInOrder(TreeNode*);
+	TreeNode* root;
+};
+#endif // !BINARY_TREE_HEADER
